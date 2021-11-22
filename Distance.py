e9a8f808b25890cd7d22900c20349a8d7ebd23d0
Dltmd202 <dltmd202@gmail.com>
2021-11-22 17:30:13 +0900

    dist

--- Distance.py
+++ Distance.py
@@ -43,10 +43,10 @@ class Distance:
                 time.sleep(0.00001)
                 gpio.output(self.trig_pin, False)
 
-            while gpio.input(self.echo_pin) == 0:
-                pulse_start = time.time()
-            while gpio.input(self.echo_pin) == 1:
-                pulse_end = time.time()
+                while gpio.input(self.echo_pin) == 0:
+                    pulse_start = time.time()
+                while gpio.input(self.echo_pin) == 1:
+                    pulse_end = time.time()
 
             pulse_duration = pulse_end - pulse_start
             distance = pulse_duration * 34000 / 2
